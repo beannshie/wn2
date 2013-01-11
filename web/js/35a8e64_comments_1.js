effb9a500e3b6e0b5d712e6a57841d947c2d1249
Zaklin Data <zaklina.data@fastwhitecat.com>
2013-01-11 16:54:42 +0100

    male zmieny w ustawieniach

--- web/js/35a8e64_comments_1.js
+++ web/js/35a8e64_comments_1.js
@@ -25,7 +25,7 @@
  *     var fos_comment_remote_cors_url = 'http://example.org/cors/index.html';
  *
  *     // Optionally set a custom callback function to update the comment count elements
- *     var window.fos_comment_thread_comment_count_callback = function(elem, threadObject){}
+ *     var fos_comment_thread_comment_count_callback = function(elem, threadObject){}
  *
  *     // Optionally set a different element than div#fos_comment_thread as container
  *     var fos_comment_thread_container = $('#other_element');
@@ -52,14 +52,19 @@
          * @param function success Optional callback function to use in case of succes.
          * @param function error Optional callback function to use in case of error.
          */
-        post: function(url, data, success, error) {
+        post: function(url, data, success, error, complete) {
             // Wrap the error callback to match return data between jQuery and easyXDM
             var wrappedErrorCallback = function(response){
                 if('undefined' !== typeof error) {
                     error(response.responseText, response.status);
                 }
             };
-            $.post(url, data, success).error(wrappedErrorCallback);
+            var wrappedCompleteCallback = function(response){
+                if('undefined' !== typeof complete) {
+                    complete(response.responseText, response.status);
+                }
+            };
+            $.post(url, data, success).error(wrappedErrorCallback).complete(wrappedCompleteCallback);
         },
 
         /**
@@ -87,16 +92,22 @@
          * @param string url Optional url for the thread. Defaults to current location.
          */
         getThreadComments: function(identifier, permalink) {
-            if('undefined' == typeof permalink) {
-                permalink = window.location.href;
-            }
+            var event = jQuery.Event('fos_comment_before_load_thread');
 
+            event.identifier = identifier;
+            event.params = {
+                permalink: encodeURIComponent(permalink || window.location.href)
+            };
+
+            FOS_COMMENT.thread_container.trigger(event);
             FOS_COMMENT.get(
-                FOS_COMMENT.base_url  + '/' + encodeURIComponent(identifier) + '/comments',
-                {permalink: encodeURIComponent(permalink)},
+                FOS_COMMENT.base_url  + '/' + encodeURIComponent(event.identifier) + '/comments',
+                event.params,
+                // success
                 function(data) {
                     FOS_COMMENT.thread_container.html(data);
-                    FOS_COMMENT.thread_container.attr('data-thread', identifier);
+                    FOS_COMMENT.thread_container.attr('data-thread', event.identifier);
+                    FOS_COMMENT.thread_container.trigger('fos_comment_load_thread', event.identifier);
                 }
             );
         },
@@ -109,23 +120,36 @@
                 'form.fos_comment_comment_new_form',
                 function(e) {
                     var that = $(this);
+                    var serializedData = FOS_COMMENT.serializeObject(this);
+
+                    e.preventDefault();
+
+                    var event = $.Event('fos_comment_submitting_form');
+                    that.trigger(event);
+
+                    if (event.isDefaultPrevented()) {
+                        return;
+                    }
 
                     FOS_COMMENT.post(
                         this.action,
-                        FOS_COMMENT.serializeObject(this),
+                        serializedData,
                         // success
                         function(data, statusCode) {
                             FOS_COMMENT.appendComment(data, that);
+                            that.trigger('fos_comment_new_comment', data);
                         },
                         // error
                         function(data, statusCode) {
                             var parent = that.parent();
                             parent.after(data);
                             parent.remove();
+                        },
+                        // complete
+                        function(data, statusCode) {
+                            that.trigger('fos_comment_submitted_form', statusCode);
                         }
                     );
-
-                    e.preventDefault();
                 }
             );
 
@@ -133,14 +157,19 @@
                 '.fos_comment_comment_reply_show_form',
                 function(e) {
                     var form_data = $(this).data();
-                    var that = this;
+                    var that = $(this);
+
+                    if(that.closest('.fos_comment_comment_reply').hasClass('fos_comment_replying')) {
+                        return that;
+                    }
 
                     FOS_COMMENT.get(
                         form_data.url,
                         {parentId: form_data.parentId},
                         function(data) {
-                            $(that).parent().addClass('fos_comment_replying');
-                            $(that).after(data);
+                            that.closest('.fos_comment_comment_reply').addClass('fos_comment_replying');
+                            that.after(data);
+                            that.trigger('fos_comment_show_form', data);
                         }
                     );
                 }
@@ -149,8 +178,16 @@
             FOS_COMMENT.thread_container.on('click',
                 '.fos_comment_comment_reply_cancel',
                 function(e) {
-                    var form_holder = $(this).parent().parent().parent();
-                    form_holder.parent().removeClass('fos_comment_replying');
+                    var form_holder = $(this).closest('.fos_comment_comment_form_holder');
+
+                    var event = $.Event('fos_comment_cancel_form');
+                    form_holder.trigger(event);
+
+                    if (event.isDefaultPrevented()) {
+                        return;
+                    }
+
+                    form_holder.closest('.fos_comment_comment_reply').removeClass('fos_comment_replying');
                     form_holder.remove();
                 }
             );
@@ -188,6 +225,7 @@
                         // success
                         function(data) {
                             FOS_COMMENT.editComment(data);
+                            that.trigger('fos_comment_edit_comment', data);
                         },
 
                         // error
@@ -212,7 +250,8 @@
             FOS_COMMENT.thread_container.on('click',
                 '.fos_comment_comment_vote',
                 function(e) {
-                    var form_data = $(this).data();
+                    var that = $(this);
+                    var form_data = that.data();
 
                     // Get the form
                     FOS_COMMENT.get(
@@ -228,6 +267,7 @@
                                 FOS_COMMENT.serializeObject(form),
                                 function(data) {
                                     $('#' + form_data.scoreHolder).html(data);
+                                    that.trigger('fos_comment_vote_comment', data, form);
                                 }
                             );
                         }
@@ -239,6 +279,13 @@
                 '.fos_comment_comment_remove',
                 function(e) {
                     var form_data = $(this).data();
+
+                    var event = $.Event('fos_comment_removing_comment');
+                    $(this).trigger(event);
+
+                    if (event.isDefaultPrevented()) {
+                        return
+                    }
 
                     // Get the form
                     FOS_COMMENT.get(
@@ -298,23 +345,29 @@
             var form_data = form.data();
 
             if('' != form_data.parent) {
-                var form_parent = form.parent();
+                var form_parent = form.closest('.fos_comment_comment_form_holder');
 
                 // reply button holder
-                var reply_button_holder = form_parent.parent();
+                var reply_button_holder = form.closest('.fos_comment_comment_reply');
+
+                var comment_element = form.closest('.fos_comment_comment_show')
+                    .children('.fos_comment_comment_replies');
+
                 reply_button_holder.removeClass('fos_comment_replying');
 
-                reply_button_holder.after(commentHtml);
+                comment_element.prepend(commentHtml);
+                comment_element.trigger('fos_comment_add_comment', commentHtml);
 
                 // Remove the form
                 form_parent.remove();
             } else {
                 // Insert the comment
                 form.after(commentHtml);
+                form.trigger('fos_comment_add_comment', commentHtml);
 
                 // "reset" the form
                 form = $(form[0]);
-                form.find('textarea').val('');
+                form[0].reset();
                 form.children('.fos_comment_form_errors').remove();
             }
         },
